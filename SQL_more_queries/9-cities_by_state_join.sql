c01f51eb9598d9a9b0b6a975785bf1c255977176
Krosenvag <herve.leguennec13@gmail.com>
2025-03-05 21:52:34 +0000

    i finnaly normally the project 2 for the Holberton School

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,9 +1,5 @@
 -- Lists all cities with their state contained in the
 -- database `hbtn_0d_usa`.
-SELECT
-	cities.id AS city_id,
-	cities.name AS city_name,
-	states.name AS state_name
-FROM cities
-JOIN states ON cities.state_id = states.id
+SELECT cities.id, cities.name, states.name
+FROM cities JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
