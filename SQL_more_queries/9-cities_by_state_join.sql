ad41a2335b0d85eddec598028e193d2037e6ce7d
Krosenvag <herve.leguennec13@gmail.com>
2025-03-05 15:11:40 +0000

    i'm done for the exercise for the project 2 just i need to finish the exercise 15 16

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,9 @@
+-- Lists all cities with their state contained in the
+-- database `hbtn_0d_usa`.
+SELECT
+	cities.id AS city_id,
+	cities.name AS city_name,
+	states.name AS state_name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
