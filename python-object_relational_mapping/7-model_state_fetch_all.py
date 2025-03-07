27d6399c65bf941b234a11ad2e19f62a72aab4bc
Krosenvag <herve.leguennec13@gmail.com>
2025-03-07 09:41:16 +0000

    correction of the exercise 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""
+This script lists all State objects
+from the database `hbtn_0e_6_usa`.
+"""
 
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlachemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
