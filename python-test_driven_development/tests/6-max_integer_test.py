3985f6645cf59291652e930beed081ddbc7a86c8
Krosenvag <herve.leguennec13@gmail.com>
2025-05-15 12:44:38 +0200

    Fix import statement in max_integer test and update compiled bytecode

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -3,8 +3,7 @@
 """Unittests for max_integer([..])."""
 
 import unittest
-max_integer = __import__('6-max_integer_test').max_integer
-
+max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     """Define unittests for max_integer([..])."""
