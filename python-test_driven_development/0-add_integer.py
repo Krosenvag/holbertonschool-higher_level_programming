260436fbb1d3492e9184cdc134ec8585122b97ee
Krosenvag <herve.leguennec13@gmail.com>
2025-05-16 10:09:22 +0200

    Refactor functions and update documentation for add_integer, matrix_divided, say_my_name, and text_indentation modules

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,29 +1,50 @@
 #!/usr/bin/python3
-"""
-This module provides a function to add two integers.
+"""A module to add two numbers
 
-The function add_integer takes two arguments and returns their sum.
-If one of the arguments is a float, it is cast to an integer.
-If the arguments are not numbers, a TypeError is raised.
+This module performs the addition operation between two numbers,
+these numbers can be integers or floats.
+
 """
 
 
 def add_integer(a, b=98):
-    """
-    Add two integers or floats, cast to integers if needed.
+    """Adds two numbers
+
+    Performs the addition between two numbers.
 
     Args:
-        a: first number (int or float)
-        b: second number (int or float), default is 98
+        a (:obj:`int, float`): The first number.
+        b (:obj:`int, float`, optional): The second number.
 
     Returns:
-        The sum of a and b after casting to integers
+        int: The result of the addition.
 
-    Raises:
-        TypeError: if a or b are not int or float
     """
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    if type(a) not in (int, float):
+        raise TypeError('a must be an integer')
+
+    if type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+
+    a = convert_to_int(a)
+    b = convert_to_int(b)
+    return a + b
+
+
+def convert_to_int(num):
+    """Cast the data type of num parameter
+
+    Convert a float number to a integer number
+
+    Args:
+        num (:obj:`int, float`): The number to cast.
+
+    Returns:
+        int: The number casted to integer.
+
+    """
+    if type(num) is float:
+        num = int(num)
+        return num
+
+    return num
--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
+"""
 
-"""Defines a name-printing function."""
+This module prints the name of a anything
+
+Output: 'My name is <first name> <last name>'
+
+    * The first name and last name must be a strings
+
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Print a name.
+    """
+
+    Prints the names
 
     Args:
-        first_name (str): The first name to print.
-        last_name (str): The last name to print.
+        first_name (str): The first name of anything.
+        last_name (str): The last name of anything.
+
     Raises:
-        TypeError: If either of first_name or last_name are not strings.
+        TypeError: If `first_name` and `last_name` aren't strings.
+
     """
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
-    
+
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+
+    if type(last_name) is not str:
+        raise TypeError('last_name must be a string')
+
+    print('My name is', first_name, last_name)
