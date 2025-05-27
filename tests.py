f84d5479ce727922a76a755c408c042f47abe001
Krosenvag <herve.leguennec13@gmail.com>
2025-05-27 14:31:21 +0200

    Add unique digit extraction function to identify non-repeating digits in an integer

--- tests.py
+++ tests.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
 
+def chiffres_uniques(n):
+    chaine = str(n)
+    compteur = {}
+    for chiffre in chaine:
+        if chiffre in compteur:
+            compteur[chiffre] +=1
+        else:
+            compteur[chiffre] = 1
+    
+    resultats = []
+    for chiffre in chaine:
+        if compteur[chiffre] == 1:
+            resultats.append(int(chiffre))
+    
+    return resultats
 
+print(chiffres_uniques(1122335))
