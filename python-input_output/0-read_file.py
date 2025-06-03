5350f1d03bd74a60d10f7d4afcf7b758c1b2b87f
Krosenvag <herve.leguennec13@gmail.com>
2025-06-03 09:59:49 +0200

    Refactor read_file function to simplify file reading and remove unnecessary docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,15 +1,5 @@
-#!usr/bin/python3
-"""Defines a function that reads text file"""
-
-
+#!/usr/bin/python3
 def read_file(filename=""):
-    """Function returns a UTF* text file
-    Args:
-        filename: name of file to read
-    Return:
-        UTF8 text file
-    """
-    with open(filename, "r", encoding="utf-8") as f:
-        read_file = f.read()
-        print(read_file, end="")
-    return read_file
+    with open(filename, encoding='utf-8') as f:
+        for line in f:
+            print(line, end='')
