dc2fbed54bc25ca0f8e3aa283ffe9e76e8358621
Krosenvag <herve.leguennec13@gmail.com>
2025-06-28 11:13:00 +0200

    Remove unnecessary blank line before session closure in city fetch script

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -22,5 +22,4 @@ if __name__ == "__main__":
                               .filter(City.state_id == State.id) \
                               .order_by(City.id):
         print("{}: ({}) {}".format(state.name, city.id, city.name))
-    
     session.close()
